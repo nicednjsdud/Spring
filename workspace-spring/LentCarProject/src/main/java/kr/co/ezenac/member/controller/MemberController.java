32bc03f65c92390a1ecf24fb27bfce7b99db3ea0
JeongWonYoung <nicednjsdud@naver.com>
2022-07-23 13:25:08 +0900

    Impl memberController
    implement memberController and memberControllerImpl

---
+++ workspace-spring/LentCarProject/src/main/java/kr/co/ezenac/member/controller/MemberController.java
@@ -0,0 +1,22 @@
+package kr.co.ezenac.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import kr.co.ezenac.member.dto.MemberDTO;
+
+public interface MemberController {
+
+	public ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView addMember(@ModelAttribute("member") MemberDTO memberDTO,HttpServletRequest request,HttpServletResponse response) throws Exception;
+	public ModelAndView removeMember(@RequestParam String id,HttpServletRequest request,HttpServletResponse response) throws Exception;
+	public ModelAndView login(@ModelAttribute("member")MemberDTO memberDTO,RedirectAttributes rAttributes,HttpServletRequest request,HttpServletResponse response) throws Exception;
+	public ModelAndView logout(HttpServletRequest request,HttpServletResponse response) throws IOException;
+}
