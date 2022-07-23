7bd3e99c051f16b51155cba545a0ba92769ec5a0
JeongWonYoung <nicednjsdud@naver.com>
2022-07-23 13:16:21 +0900

    Impl Member Repository
    implement memberRepository about shopping mall

--- workspace-spring/ezen13_store/src/main/java/kr/co/bobstudy/member/dao/MemberDAO.java
+++ workspace-spring/ezen13_store/src/main/java/kr/co/bobstudy/member/dao/MemberDAO.java
@@ -1,9 +1,12 @@
 package kr.co.bobstudy.member.dao;
 
+import java.util.Map;
+
 import org.springframework.dao.DataAccessException;
 
 public interface MemberDAO {
 
 	public String selectDuplicateCheck(String id) throws DataAccessException;
 
+	public int insertMember(Map memberMap)throws DataAccessException;
 }
--- workspace-spring/ezen13_store/src/main/java/kr/co/bobstudy/member/dao/MemberDAOImpl.java
+++ workspace-spring/ezen13_store/src/main/java/kr/co/bobstudy/member/dao/MemberDAOImpl.java
@@ -1,5 +1,7 @@
 package kr.co.bobstudy.member.dao;
 
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -16,5 +18,12 @@ public class MemberDAOImpl implements MemberDAO{
 		String result = sqlSession.selectOne("mapper.member.duplicatecheck",id);
 		return result;
 	}
+
+	@Override
+	public int insertMember(Map memberMap) throws DataAccessException {
+		int result = sqlSession.insert("mapper.member.insertMember",memberMap);
+		return result;
+	}
+	
 
 }
